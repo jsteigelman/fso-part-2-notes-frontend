d9f977185045d3478e3dc0a6d8ae7753be593514
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-05 10:58:23 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -54,10 +54,15 @@ const App = () => {
     const changedNote = { ...note, important: !note.important }
 
     noteService
-    .update(id, changedNote)
-    .then(returnedNote => {
+    .update(id, changedNote).then(returnedNote => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-  })
+    })
+    .catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
   }
 
   
