e7c05de51988ed4198c11eff5e75317f154c5ff5
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-07 13:17:00 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -55,11 +55,13 @@ const App = () => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
+
     noteService
     .update(id, changedNote).then(returnedNote => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
     .catch(error => {
+      console.log(error)
       setErrorMessage(
         `Error: The note '${note.content}' was already removed from server`
       )
