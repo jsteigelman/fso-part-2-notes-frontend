48913b86e2b4fe39f5c69d7b2bcefee052ae7356
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-18 18:05:32 -0400

    fixed route handler

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/api/notes')
+      .get('/api/notes')
       .then(res => {
         setNotes(res.data)
       })
